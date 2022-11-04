862493c43d59124f718024405eb432f7ef9ad84d
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2022-11-04 12:26:36 +0000

    Add template for React HTML

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core'
+import {Add as AddIcon, PictureInPicture as PictureInPictureIcon,} from '@material-ui/icons'
+import 'fontsource-roboto'
 import './popup.css'
 
 const App: React.FC<{}> = () => {
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container justify="space-evenly">
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <InputBase
+                placeholder="Add a city name"
+                value={cityInput}
+                onChange={(event) => setCityInput(event.target.value)}
+              />
+              <IconButton onClick={handleCityButtonClick}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper>
+            <Box py="4px">
+              <IconButton onClick={handleTempScaleButtonClick}>
+                {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper>
+            <Box py="4px">
+              <IconButton onClick={handleOverlayButtonClick}>
+                <PictureInPictureIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {options.homeCity != '' && (<WeatherCard city={options.homeCity} tempScale={options.tempScale} />)}
+      {cities.map((city, index) => (
+        <WeatherCard
+          city={city}
+          tempScale={options.tempScale}
+          key={index}
+          onDelete={() => handleCityDeleteButtonClick(index)}
+        />
+      ))}
+      <Box height="16px" />
+    </Box>
   )
 }
 
