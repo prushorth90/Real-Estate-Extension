74328104bb5d2a7900fc042bb8f54a59b8a95430
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2022-11-04 12:51:32 +0000

    Add base select menu from materials ui

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,54 +4,45 @@ import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core'
 import {Add as AddIcon, PictureInPicture as PictureInPictureIcon,} from '@material-ui/icons'
 import 'fontsource-roboto'
 import './popup.css'
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 
 const App: React.FC<{}> = () => {
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+  const classes = useStyles();
+
+  // case1: return if nothing
   return (
     <Box mx="8px" my="16px">
-      <Grid container justify="space-evenly">
-        <Grid item>
-          <Paper>
-            <Box px="15px" py="5px">
-              <InputBase
-                placeholder="Add a city name"
-                value={cityInput}
-                onChange={(event) => setCityInput(event.target.value)}
-              />
-              <IconButton onClick={handleCityButtonClick}>
-                <AddIcon />
-              </IconButton>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item>
-          <Paper>
-            <Box py="4px">
-              <IconButton onClick={handleTempScaleButtonClick}>
-                {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
-              </IconButton>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item>
-          <Paper>
-            <Box py="4px">
-              <IconButton onClick={handleOverlayButtonClick}>
-                <PictureInPictureIcon />
-              </IconButton>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
-      {options.homeCity != '' && (<WeatherCard city={options.homeCity} tempScale={options.tempScale} />)}
-      {cities.map((city, index) => (
-        <WeatherCard
-          city={city}
-          tempScale={options.tempScale}
-          key={index}
-          onDelete={() => handleCityDeleteButtonClick(index)}
-        />
-      ))}
-      <Box height="16px" />
+    <FormControl className={classes.formControl}>
+     <InputLabel id="demo-simple-select-helper-label">Topic</InputLabel>
+     <Select
+       labelId="demo-simple-select-helper-label"
+       id="demo-simple-select-helper"
+       // value={age}
+       // onChange={handleChange}
+     >
+       <MenuItem value="">
+         <em>None</em>
+       </MenuItem>
+       <MenuItem value={10}>Ten</MenuItem>
+       <MenuItem value={20}>Twenty</MenuItem>
+       <MenuItem value={30}>Thirty</MenuItem>
+     </Select>
+   </FormControl>
     </Box>
   )
 }
