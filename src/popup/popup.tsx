6cea173255958f618c92e3319cf790fc10314147
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2022-11-13 09:34:03 +0000

    Clean up popup extract url code

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -11,59 +11,15 @@ import RestaurantPopup from '../components/RestaurantPopup'
 const App: React.FC<{}> = () => {
   const [topic, setTopic] = useState<Topic>(Topic.Topics)
   const [addr, setAddr] = useState<string>()
-  //https://stackoverflow.com/questions/71834064/react-chrome-extension-how-to-modify-content-of-popup-on-url-change
+
   useEffect(() => {
-    // chrome.storage.local.get("x", (res) => {
-    // //   console.log(res)
-    //      let m=res
-    //      setAddr(m.x)
-    //   })
-    // });
-    var query = { active: true, lastFocusedWindow: true}
-    function callback(tabs) {
-    var currentTab = tabs[0]; // there will be only one in this array
-      console.log(currentTab.url); // also has properties like currentTab.id
-      setAddr(currentTab.url)
-
-    }
-    chrome.tabs.query(query, callback)
+    chrome.tabs.query({ active: true, lastFocusedWindow: true}, (tabs)=> {
+      let currentTab = tabs[0];
+        console.log(currentTab.url); 
+        setAddr(currentTab.url)
+    })
   })
-  // 1. WEIRD ONLY WORKS WHEN REFRESH???? - bidrecrional/POPUP ONLY LISTEN ONCE
 
-  // chrome.runtime.onMessage.addListener((msg,sender,sendResponse) => {
-  //   //console.log("W2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2SW2S")
-  //   console.log(msg)
-  //   let pol : string = msg
-  //   setAddr(pol)
-  // })
-
-  // 2. main problem: popup needs url of curent page. this works only when clicking the popup again but wont if leave it open
-  // -https://stackoverflow.com/questions/68747451/how-can-i-close-a-popup-window-when-the-url-changes
-  //https://stackoverflow.com/questions/13359421/chrome-extension-get-current-tab-from-popup
-  // or instead currwindow uuse lastFocusedWindow
-  // potential solution: make popup close on change webpage
-  // how does state change for popup
-
-  // var query = { active: true, lastFocusedWindow: true}
-  // function callback(tabs) {
-  // var currentTab = tabs[0]; // there will be only one in this array
-  //   console.log(currentTab.url); // also has properties like currentTab.id
-  // }
-  //<div> {chrome.tabs.query(query, callback)}
-
-
-
-
-  // chrome.tabs.query(query, callback);
-  //chrome.tabs.onActivated.addListener(callback)
-  //3.
-  // chrome.storage.local.get(["x"], (res) => {
-  //   console.log(res)
-  //   let m=res
-  //   setAddr(m)
-  // })
-  //console.log("KSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSIKSI")
-  // 1. check if popup can extract page url directly 2. watch udemy 3. use webpage content not url
   // case1: return if nothing card foreach api?????? 1 topic to many api and dataset
   return (
     <Box mx="8px" my="16px">
