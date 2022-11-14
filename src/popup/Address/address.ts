422acfeb10ae0b0ffa8346f91de383955aa06391
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2022-11-14 10:18:56 +0000

    Add Alternate way to update address

--- src/popup/Address/address.ts
+++ src/popup/Address/address.ts
@@ -1,6 +1,55 @@
-export interface Address {
-  street: string
-  city: string
-  state: string
-  zipCode: string
+export class Address {
+  // could make obj for apartment like open weather data
+  private street: string
+  private city: string
+  private state: string
+  private zipCode: string
+
+  public constructor(url: string) {
+    let splitAddressURL = this.divideURL(url)
+    this.street = splitAddressURL[0]
+    this.city = splitAddressURL[1]
+    this.state = splitAddressURL[2]
+    this.zipCode = splitAddressURL[3]
+
+  }
+
+  public getStreet() : string {
+    return this.street
+  }
+  public getCity() : string {
+    return this.city
+  }
+  public getState() : string {
+    return this.state
+  }
+  public getZipCode() : string {
+    return this.zipCode
+  }
+  private divideURL(url: string) : string[]{
+    let addressURL = this.removeSubURL(url)
+    let splitAddressURL = this.splitAddressURL(addressURL)
+    return splitAddressURL
+  }
+
+  private removeSubURL(url: string): string {
+    //"https://www.realtor.com/realestateandhomes-detail/1414-170th-Pl-NE_Bellevue_WA_98008_M13563-86961"
+    let detailIndex = url.indexOf("detail")
+    //detail/1414-170th-Pl-NE_Bellevue_WA_98008_M13563-86961
+    let subURL = url.slice(detailIndex,url.length)
+    let slashIndex = subURL.indexOf("/")
+    // 1414-170th-Pl-NE_Bellevue_WA_98008_M13563-86961
+    let addressURL= subURL.slice(slashIndex+1,subURL.length)
+    return addressURL
+  }
+
+  private splitAddressURL(addressURL: string): string[] {
+    let splitAddressURL = addressURL.split("_")
+    splitAddressURL.pop()
+    return splitAddressURL
+    // 0:"1414-170th-Pl-NE"
+    // 1:"Bellevue"
+    // 2:"WA"
+    // // 3:"98008"
+  }
 }
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -8,17 +8,11 @@ import TopicMenu from '../components/TopicMenu'
 import WeatherPopup from '../components/WeatherPopup'
 import RestaurantPopup from '../components/RestaurantPopup'
 import {Address} from './Address/address'
-import {RealtorAddress} from './Address/realtorAddress'
 
 // https://v4.mui.com/components/selects/
 const App: React.FC<{}> = () => {
   const [topic, setTopic] = useState<Topic>(Topic.Topics)
-  const [addr, setAddr] = useState<Address>({
-    street: '',
-    city: '',
-    state: '',
-    zipCode: '',
-  })
+  const [addr, setAddr] = useState<Address>(new Address(""))
 
   // Get the url from the current tab
   useEffect(() => {
@@ -26,15 +20,9 @@ const App: React.FC<{}> = () => {
       let currentTab = tabs[0];
       console.log(currentTab.url);
       //mayve if to check in manifest settings which url
-      let p = new RealtorAddress(currentTab.url)
-      const updatedAddr: Address = {
-        street: p.getStreet(),
-        city: p.getCity(),
-        state: p.getState(),
-        zipCode: p.getZipCode(),
-      }
-      setAddr(updatedAddr)
-      console.log(addr.street)
+      let updatedAddress = new Address(currentTab.url)
+      setAddr(updatedAddress)
+      console.log(updatedAddress.getStreet())
       //setAddr(currentTab.url)
     })
   }, [])
@@ -43,9 +31,9 @@ const App: React.FC<{}> = () => {
   return (
     <Box mx="8px" my="16px">
       <TopicMenu topic={topic} setTopic={setTopic}/>
-      <WeatherPopup topic={topic} city={addr.city}/>
+      <WeatherPopup topic={topic} city={addr.getCity()}/>
       <RestaurantPopup topic={topic}/>
-      <div>{addr.zipCode}</div>
+      <div>{addr.getZipCode()}</div>
     </Box>
 
   )
