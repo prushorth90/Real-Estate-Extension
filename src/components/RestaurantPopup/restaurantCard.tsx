0a8c88abd800c97ba630e7e6b44e9129a53e4864
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2022-11-21 16:02:44 +0000

    Add rating system to restaurant

--- src/components/RestaurantPopup/restaurantCard.tsx
+++ src/components/RestaurantPopup/restaurantCard.tsx
@@ -7,6 +7,7 @@ import {RestaurantCardContainer} from './restaurantCardContainer'
 import {Address} from '../../popup/Address/address'
 import {AddressData,AddressAPI} from '../../utils/api/address/addressIndex'
 import {PhotoDialog} from './dialog/photoDialog'
+import Rating from '@material-ui/lab/Rating';
 
 const RestaurantCard: React.FC<{coord
 }> = ({ coord}) => {
@@ -49,27 +50,25 @@ const RestaurantCard: React.FC<{coord
   }
 
   return (
-    <div>
+    <Box>
     {console.log("12")}
         {nearbySearchData.results.map((result, index) => (
           <RestaurantCardContainer key={index}>
-            <Grid container justifyContent="space-around">
+            <Grid container>
               <Grid item>
-                <div>{index}</div>
                 <Typography className="restaurantCard-title"> {result.name} </Typography>
-                <Typography className="restaurantCard-temp"> Ratings: {result.user_ratings_total} </Typography>
-                <Typography className="restaurantCard-body"> PriceLevel: {result.price_level} </Typography>
-                <Typography className="restaurantCard-body"> URT: {result.user_ratings_total} </Typography>
+                <Typography className="restaurantCard-body" component="legend">Total User Ratings: {result.user_ratings_total} </Typography>
+                <Rating name="read-only" className="restaurantCard-body" value={result.rating} readOnly />
+                <Typography className="restaurantCard-body"> Price Level: {result.price_level} </Typography>
                 <Typography className="restaurantCard-body"> Vicinity: {result.vicinity} </Typography>
-                <Button key={result.photos !== undefined ? result.photos[0].photo_reference: null} variant="outlined" color="primary" onClick={() => {
+                <br/>
+                <br/>
+                <Button className="restaurantCard-body" key={result.photos !== undefined ? result.photos[0].photo_reference: null} variant="outlined" color="primary" onClick={() => {
                   if (result.photos !== undefined) {
                     setPhotoReference(result.photos[0].photo_reference)
-                    console.log("ao")
                   }
-                  console.log("bo")
                   setOpenPhoto(true)
                   setCurrIndex(index)
-                  console.log("co")
                 }}> View Photo </Button>
                 {index == currIndex ? <PhotoDialog open={openPhoto} onClose={() => setOpenPhoto(false)} photo_reference={photoReference}/> : null}
               </Grid>
@@ -77,9 +76,62 @@ const RestaurantCard: React.FC<{coord
           </RestaurantCardContainer>
 
         ))}
-      </div>
+      </Box>
   )
 }
 
 export default RestaurantCard
 // some results no photos field
+
+
+// import React, { useEffect, useState } from 'react'
+// import { Box, Button, Card, CardActions, CardContent, Grid, Typography,} from '@material-ui/core'
+// import { RestaurantAPI, NearbySearchData } from '../../utils/api/restaurant/restaurantIndex'
+// import './restaurantCard.css'
+// import {RestaurantCardState} from './restaurantCardState'
+// import {RestaurantCardContainer} from './restaurantCardContainer'
+// import {Address} from '../../popup/Address/address'
+// import {AddressData,AddressAPI} from '../../utils/api/address/addressIndex'
+// import {PhotoDialog} from './dialog/photoDialog'
+// import Rating from '@material-ui/lab/Rating';
+//
+// const RestaurantCard: React.FC<{coord
+// }> = ({ coord}) => {
+//   console.log("9")
+//   const [nearbySearchData, setNearbySearchData] = useState<NearbySearchData | null>(null)
+//   const [cardState, setCardState] = useState<RestaurantCardState>(RestaurantCardState.Loading)
+//   const [openPhoto, setOpenPhoto] = useState<boolean>(false);
+//   const [photoReference, setPhotoReference] = useState<string>("");
+//   const [currIndex, setCurrIndex ] = useState<number>(-1);
+//
+//   let restaurantApi = new RestaurantAPI()
+//
+//   return (
+//     <div>
+//     {console.log("12")}
+//         {[9,8,7].map((result, index) => (
+//           <RestaurantCardContainer key={index}>
+//             <Grid container justifyContent="space-around">
+//               <Grid item>
+//                 <div>{index}</div>
+//                 <Typography className="restaurantCard-title"> ibvnv </Typography>
+//                 <Typography className="restaurantCard-body"> 8 </Typography>
+//                 <Rating name="read-only" className="restaurantCard-body" value={90} readOnly />
+//                 <Typography className="restaurantCard-body"> 900 </Typography>
+//                 <Typography className="restaurantCard-body"> Vicinity: "popopopopo" </Typography>
+//               </Grid>
+//               <Grid item>
+//                 <Button variant="outlined" color="primary"> View Photo </Button>
+//                 <br></br>
+//                 <br></br>
+//                 <Button variant="outlined" color="primary"> View Photo </Button>
+//               </Grid>
+//             </Grid>
+//           </RestaurantCardContainer>
+//
+//         ))}
+//       </div>
+//   )
+// }
+//
+// export default RestaurantCard
