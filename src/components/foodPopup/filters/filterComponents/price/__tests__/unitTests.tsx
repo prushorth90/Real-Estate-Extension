5e3776996348a20cae6e029f3b5e6030b5950380
Prushorth Manivannan <manivanann.prushorth@kcl.ac.uk>
2023-02-11 22:18:22 +0000

    Add unit event tests for changing value of price filters

--- src/components/foodPopup/filters/filterComponents/price/__tests__/unitTests.tsx
+++ src/components/foodPopup/filters/filterComponents/price/__tests__/unitTests.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "@testing-library/jest-dom/extend-expect"
-import { act, screen, render, fireEvent, waitFor, cleanup } from "@testing-library/react";
+import { act, screen, render, fireEvent, waitFor, cleanup,within, getByTestId } from "@testing-library/react";
 import FoodPopup from "../../../../foodPopup";
 import {MaxPriceFilter} from '../maxPriceFilter'
 import {MinPriceFilter} from '../MinPriceFilter'
-import { TopicContext } from '../../../../../../popup/popup'
+import App, { TopicContext } from '../../../../../../popup/popup'
 import { APIContext } from '../../../filters'
 import {APIInput} from '../../../apiInput'
+import UserEvent from '@testing-library/user-event'
 
 describe("Components Render", () => {
 
@@ -40,7 +41,7 @@ describe("Filters UI in doc", () => {
 
 describe("Filters UI visible", () => {
 
-    it("should render Max Price Filter ", () => {
+    it("should show the max filter to user ", () => {
         render(<APIContext.Provider value={[new APIInput()]}> <MaxPriceFilter /></APIContext.Provider>)
 
         const maxPriceLevel = screen.getByTestId("Max Price Level") as HTMLSelectElement
@@ -48,7 +49,7 @@ describe("Filters UI visible", () => {
         expect(maxPriceLevel).toBeVisible()
     });
 
-    it("should render Min Price Filter", () => {
+    it("should show the min filter to user ", () => {
         render(<APIContext.Provider value={[new APIInput()]}> <MinPriceFilter /></APIContext.Provider>)
 
         const minPriceLevel = screen.getByTestId("Min Price Level") as HTMLSelectElement
@@ -59,7 +60,7 @@ describe("Filters UI visible", () => {
 
 describe("UI values", () => {
 
-    it("should render Max Price Filter ", () => {
+    it("should show the value of max filter to user", () => {
         render(<APIContext.Provider value={[new APIInput()]}> <MaxPriceFilter /></APIContext.Provider>)
 
         const maxPriceLevel = screen.getByTestId("Input Max Price Level") as HTMLInputElement
@@ -67,11 +68,67 @@ describe("UI values", () => {
         expect(maxPriceLevel.value).toBe("4")
     });
 
-    it("should render Min Price Filter", () => {
+    it("should show the value of min filter to user ", () => {
         render(<APIContext.Provider value={[new APIInput()]}> <MinPriceFilter /></APIContext.Provider>)
 
         const minPriceLevel = screen.getByTestId("Input Min Price Level") as HTMLInputElement
 
         expect(minPriceLevel.value).toBe("0")
     });
+});
+
+
+describe("Event test change value of filter", () => {
+
+    it("should change from 4 to 3 ", () => {
+        const { getByTestId, getAllByRole } = render((<TopicContext.Provider value={["Food", jest.fn()]}> <FoodPopup /></TopicContext.Provider>))
+
+        const maxPriceLevel = screen.getByTestId("Input Max Price Level") as HTMLSelectElement
+        fireEvent.mouseDown(getAllByRole('button')[3]);
+        //expect(maxPriceLevel).toBeInTheDocument()
+        const options = within(screen.getByRole('listbox'));
+        fireEvent.click(options.getByText(/3/i));
+        expect(maxPriceLevel.value).toBe("3")
+    });
+
+    it("should change from 4 to 3 to 4", () => {
+        const { getByTestId, getAllByRole } = render((<TopicContext.Provider value={["Food", jest.fn()]}> <FoodPopup /></TopicContext.Provider>))
+
+        const maxPriceLevel = screen.getByTestId("Input Max Price Level") as HTMLSelectElement
+        fireEvent.mouseDown(getAllByRole('button')[3]);
+        //expect(maxPriceLevel).toBeInTheDocument()
+        const options = within(screen.getByRole('listbox'));
+        fireEvent.click(options.getByText(/3/i));
+        expect(maxPriceLevel.value).toBe("3")
+
+        fireEvent.click(options.getByText(/4/i));
+        expect(maxPriceLevel.value).toBe("4")
+    });
+
+    it("should change from 0 to 1 ", () => {
+        const { getByTestId, getAllByRole } = render((<TopicContext.Provider value={["Food", jest.fn()]}> <FoodPopup /></TopicContext.Provider>))
+
+        const minPriceLevel = screen.getByTestId("Input Min Price Level") as HTMLSelectElement
+        fireEvent.mouseDown(getAllByRole('button')[2]);
+        //expect(maxPriceLevel).toBeInTheDocument()
+        const options = within(screen.getByRole('listbox'));
+        fireEvent.click(options.getByText(/1/i));
+        expect(minPriceLevel.value).toBe("1")
+    });
+
+    it("should change from 0 to 1 to 0", () => {
+        const { getByTestId, getAllByRole } = render((<TopicContext.Provider value={["Food", jest.fn()]}> <FoodPopup /></TopicContext.Provider>))
+
+        const minPriceLevel = screen.getByTestId("Input Min Price Level") as HTMLSelectElement
+        fireEvent.mouseDown(getAllByRole('button')[2]);
+        //expect(maxPriceLevel).toBeInTheDocument()
+        const options = within(screen.getByRole('listbox'));
+        fireEvent.click(options.getByText(/1/i));
+        expect(minPriceLevel.value).toBe("1")
+
+        fireEvent.click(options.getByText(/0/i));
+        expect(minPriceLevel.value).toBe("0")
+    });
+
+
 });
